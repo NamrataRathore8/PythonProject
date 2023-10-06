55adcfd441335d15140480f1e467c0276577af5e
NamrataRathore8 <87515296+NamrataRathore8@users.noreply.github.com>
2023-10-06 07:16:14 +0530

    Add files via upload

---
+++ Lab04.py
@@ -0,0 +1,14 @@
+#variable store value
+# Variable is a containe
+# Variable Name = Variable Value
+
+#pi = 3.14
+#print(type(pi))
+
+name = input("Enter your name")
+print(name)
+
+firstName = input("Enter first name")
+lastName = input("Enter last name")
+print(firstName,lastName,sep = "-")
+print(firstName,",you are welcome to python class")
